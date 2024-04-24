c2d1abb07508d04a756dcf3bb8ac6d8084183446
Lisa Mure <melisamureriwa@gmail.com>
2024-04-24 15:21:39 +0200

    Implemented _removeItem method in App class to remove deleted items

--- app.js
+++ app.js
@@ -7,33 +7,68 @@ class CalorieTracker {
 
         this._displayCalorieLimit()
         this._displayTotalCalories()
+        this._displayCaloriesConsumed()
+        this._displayCaloriesBurned()
+        this._displayCaloriesRemaining()
 
     }
 
+    // Public Methods
+
+    // Add meal
     addMeal(meal) {
         this._meals.push(meal)
         this._totalCalories += meal.calories
+        this._displayNewMeal(meal)
         this._render()
     }
 
+    // Add Workout
     addWorkout(workout) {
         this._workouts.push(workout)
         this._totalCalories -= workout.calories
+        this._displayNewWorkout(workout)
         this._render()
     }
 
+    // Remove Meal
+    removeMeal(id) {
+       const index = this._meals.findIndex(meal => meal.id === id)
+            if (index !== -1) {
+               const removedMeal = this._meals.splice(index, 1)[0];
+               this._totalCalories -= removedMeal.calories;
+               this._render()
+            }
+    }
+
+    
+    // Remove Workout
+    removeWorkout(id) {
+       const index = this._workouts.findIndex(workout => workout.id === id)
+            if (index !== -1) {
+               const removedWorkout = this._workouts.splice(index, 1)[0];
+               this._totalCalories -= removedWorkout.calories;
+               this._render()
+            }
+    }
+
+    // Private Methods
+
+    // Display Calorie Limit
     _displayCalorieLimit() {
         const calorieLimitEl = document.getElementById("calorie-limit")
 
         calorieLimitEl.innerHTML = this._calorieLimit
     }
 
+    // Display Total Calories
     _displayTotalCalories() {
         const totalCaloriesEl = document.getElementById("calorie-total")
 
         totalCaloriesEl.innerHTML = totalCaloriesEl.innerHTML = this._totalCalories; 
     }
 
+    // Display Calories Consumed
     _displayCaloriesConsumed() {
         const caloriesConsumed = document.getElementById("calories-consumed")
 
@@ -42,6 +77,7 @@ class CalorieTracker {
         caloriesConsumed.innerHTML = totalCalories
     }
 
+    // Display Calories Burned
     _displayCaloriesBurned() {
         const caloriesBurned = document.getElementById("calories-burned")
         
@@ -50,6 +86,7 @@ class CalorieTracker {
         caloriesBurned.innerHTML = totalCalories
     }
 
+    // Display Calories Remaining
     _displayCaloriesRemaining() {
         const caloriesRemaining = document.getElementById("calories-remaining")
         const progressBar = document.getElementById("progress-bar")
@@ -73,6 +110,7 @@ class CalorieTracker {
         }
     }
 
+    // Update Progress Bar
     _updateProgressBar() {
         const progressBar = document.getElementById("progress-bar")
         const percentage = (this._totalCalories / this._calorieLimit) * 100
@@ -81,7 +119,48 @@ class CalorieTracker {
         progressBar.style.width = `${width}%`
     }
 
-  _render() {
+    // Display New Meal
+    _displayNewMeal(meal) {
+        const newMeal = document.getElementById("new-meal-item")
+        const mealEl = document.createElement("div")
+        mealEl.classList.add("card", "w-100", "meals-item", "mt-3") 
+        // Set attribute for id to be used when removing item
+        mealEl.setAttribute('data-id', meal.id);
+
+        mealEl.innerHTML = `
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <p>${meal.name}</p>
+            <button class="btn meal-calories-btn px-5 py-0">${meal.calories}</button>
+            <p class="m-0"> 
+                <i class="fa-solid fa-square-xmark fa-lg"></i>
+            </p>
+        </div>
+    `;
+    newMeal.appendChild(mealEl);
+    }
+
+    // Display New Workout
+    _displayNewWorkout(workout) {
+        const newWorkout = document.getElementById("new-workout-item")
+        const workoutEl = document.createElement("div")
+        workoutEl.classList.add("card", "w-100", "meals-item", "mt-3") 
+        // Set attribute for id to be used when removing item
+        workoutEl.setAttribute('data-id', workout.id);
+
+        workoutEl.innerHTML = `
+        <div class="card-body d-flex justify-content-between align-items-center">
+            <p>${workout.name}</p>
+            <button class="btn workout-calories-btn px-5 py-0">${workout.calories}</button>
+            <p class="m-0"> 
+                <i class="fa-solid fa-square-xmark fa-lg"></i>
+            </p>
+        </div>
+    `;
+    newWorkout.appendChild(workoutEl);
+    }
+
+//   To re-render the methods
+    _render() {
     this._displayCalorieLimit()
     this._displayTotalCalories()
     this._displayCaloriesConsumed()
@@ -93,16 +172,16 @@ class CalorieTracker {
 
 class Meal {
    constructor(meal, calories) {
-    this.id =  this.id = Math.random().toString(16).slice(2);
-    this.meal = meal;
+    this.id = Math.random().toString(16).slice(2);
+    this.name = meal;
     this.calories = calories
    }
 }
 
 class Workout {
     constructor(workout, calories) {
-    this.id =  this.id = Math.random().toString(16).slice(2);
-    this.workout = workout;
+    this.id = Math.random().toString(16).slice(2);
+    this.name = workout;
     this.calories = calories
     }
 }
@@ -115,39 +194,61 @@ class App {
         document.getElementById("meal-form").addEventListener("submit", this._newItem.bind(this, "meal"));
 
         document.getElementById("workout-form").addEventListener("submit", this._newItem.bind(this, "workout"));
+
+        document.getElementById("new-meal-item").addEventListener("click", this._removeItem.bind(this, "meal"))
+
+        document.getElementById("new-workout-item").addEventListener("click", this._removeItem.bind(this, "workout"))
     }
 
-    // Add New Meal
-    _newItem(type, event) {
-        event.preventDefault();
-        const name = document.getElementById(`${type}-name`);
-        const calories = document.getElementById(`${type}-calories`);
+    // Add New Item
+ _newItem(type, event) {
+    event.preventDefault();
+    const name = document.getElementById(`${type}-name`);
+    const calories = document.getElementById(`${type}-calories`);
 
-        // Validate Input 
-        if (name.value === "" || calories.value === "") {
-            alert("Please fill in all fields");
-            return;
-        }
-
-        // Check If Meal or Workout
-        if(type === "meal") {
-            const meal = new Meal(name.value, +calories.value);
-            this._tracker.addMeal(meal);
-        } else if (type === "workout") {
-            const workout = new Workout(name.value, +calories.value);
-            this._tracker.addWorkout(workout);
-
-        }
-
-        // Clear Form
-        name.value = ""
-        calories.value = ""
-        
-        // Collapse Form
-        const collapseEl = document.getElementById(`collapse-${type}`)
-        collapseEl.classList.remove("show")
-        collapseEl.setAttribute("aria-expanded", "false")
+    // Validate Input 
+    if (name.value === "" || calories.value === "") {
+        alert("Please fill in all fields");
+        return;
     }
+
+    // Check If Meal or Workout
+    if(type === "meal") {
+        const meal = new Meal(name.value, +calories.value);
+        this._tracker.addMeal(meal);
+    } else if (type === "workout") {
+        const workout = new Workout(name.value, +calories.value);
+        this._tracker.addWorkout(workout);
+    }
+
+    // Clear Form
+    name.value = ""
+    calories.value = ""
+    
+    // Collapse Form
+    const collapseEl = document.getElementById(`collapse-${type}`);
+    collapseEl.classList.remove("show");
+    collapseEl.setAttribute("aria-expanded", "false");
+}
+
+// Remove Item
+_removeItem(type, event) {
+if (event.target.classList.contains("fa-square-xmark")) {
+    const item = event.target.closest(".card")
+    const id = item.getAttribute("data-id")
+
+    if (confirm("Are you sure you want to delete this item")) {
+
+        if (type === "meal") {
+            this._tracker.removeMeal(id)
+           } else if (type === "workout") {
+            this._tracker.removeWorkout(id)
+           }
+    }
+    item.remove()
+}
+}
+
 }
 
 const app = new App();
